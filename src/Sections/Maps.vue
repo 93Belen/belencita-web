c038589c7f66e2f56acc41c44bcb1c9c88674118
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-11-17 17:24:16 -0600

    fixing

--- src/Sections/Maps.vue
+++ src/Sections/Maps.vue
@@ -12,14 +12,14 @@ register();
 
 <template>
     <div class="bg-white h-fit min-h-[80vh] border-b-2 border-black md:grid md:grid-rows-1 md:grid-cols-[60%_40%]">
-        <div class="md:hidden p-2 md:p-20 h-fit">
+        <div class="md:hidden p-2 pt-5 md:p-20 h-fit">
             <!-- Slider main container -->
             <swiper-container class="mySwiper h-fit w-[90%] m-auto" effect="cards" grab-cursor="true">
             <swiper-slide class="flex p-2 justify-center items-center backdrop-blur-sm bg-white/50 w-[90%] min-h-[600px] flex-col h-full rounded-xl border-2 border-black">
                 <Spain/>
                 <div class="pt-2">
                     <h3 class="font-bold text-sm">Born and raised in the southernmost part of Spain.</h3>
-                    <p class="text-sm">Here I learned the Mediterranean values that I hold closest to my heart: 
+                    <p class="text-xs">Here I learned the Mediterranean values that I hold closest to my heart: 
                     to enjoy the little things in life and take care of those around you, 
                     because these are the only things that truly matter.
                     Almeria is the humble crossroads between Africa and Europe. I miss the narrow streets, 
@@ -30,7 +30,7 @@ register();
                 <Spain/>
                 <div class="pt-2">
                     <h3 class="font-bold text-sm">At 18, I moved to Granada to study Philosophy at university.</h3>
-                    <p class="text-sm"> This city is the true jewel of Andalucía’s Arab past. 
+                    <p class="text-xs"> This city is the true jewel of Andalucía’s Arab past. 
                     Here I learned about Nietzsche, I made friends, we moved into a cave/house - google it - and I was happy.
                     But here I came to the hard realization that I wouldn’t have a future if I stayed in Spain. 
                     I had no money, neither hope, nor opportunities.</p>
@@ -40,7 +40,7 @@ register();
                 <Uk/>
                 <div class="p-2">
                     <h3 class="font-bold text-sm">London changed everithing.</h3>
-                    <p class="text-sm">
+                    <p class="text-xs">
                     I didn’t speak English when I arrived, but I was in my early twenties and ready for the adventure. 
                     Here I first saw a future for myself; I started cleaning tables at Whole Foods and built a career in retail.
                     I made friends from all over the world, and met my husband. London enriched my world and the possibilities stored for me.
@@ -51,7 +51,7 @@ register();
                 <Us/>
                 <div class="p-2">
                     <h3 class="font-bold text-sm">Now I live in Kansas City...</h3>
-                    <p class="text-sm">
+                    <p class="text-xs">
                         And although it took me some time to adjust to the cultural shifts, 
                         I can proudly say that I’ve come to call it home. Here, I’ve truly taken control of my future. 
                         I’ve worked hard to build a career in tech, and I’ve been given opportunities I never could have dreamed of.
@@ -66,7 +66,7 @@ register();
 
 <style scoped>
 swiper-slide {
-    background-image: url('./bg.png');
+    background-image: url('/bg.png');
     background-size: cover;
 }
 </style>
