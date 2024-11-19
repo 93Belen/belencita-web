1451b5e98849f4f5c212f9a8dee9c4ddea7ef79e
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-11-18 20:41:31 -0600

    tabs

--- src/Sections/Fidgets.vue
+++ src/Sections/Fidgets.vue
@@ -3,16 +3,36 @@
 </script>
 
 <template>
-    <div class="h-screen bg-white pt-10">
-        <div role="tablist" class="tabs tabs-lifted">
-            <div role="tab" class="tab bg-purple"></div>
-            <div role="tab" class="tab bg-yellow"></div>
-            <div role="tab" class="tab bg-orange"></div>
-            <div role="tab" class="tab bg-pink"></div>
-            <div role="tab" class="tab bg-blue"></div>
-        </div>
-        <div class="h-90vh">
+    <div class="h-screen w-screen bg-white pt-10">
+        <div role="tablist" class="tabs tabs-lifted md:hidden grid-cols-5">
+            <input type="radio" name="my_tabs_2" role="tab" class="tab tab-active bg-purple [--tab-bg:#CEBAF0] [--tab-border-color:transparent]" aria-label="" />
+            <div role="tabpanel" class="tab-content bg-purple p-6 h-[90vh]">
+                Tab content 1
+            </div>
 
+            <input type="radio" name="my_tabs_2" role="tab" class="tab bg-yellow [--tab-bg:#FCF6BD] [--tab-border-color:transparent]"  aria-label="" />
+            <div role="tabpanel" class="tab-content bg-yellow p-6 h-[90vh]">
+                Tab content 2
+            </div>
+
+            <input type="radio" name="my_tabs_2" role="tab" class="tab bg-orange [--tab-bg:#FDE3CE] [--tab-border-color:transparent]" aria-label="" />
+            <div role="tabpanel" class="tab-content bg-orange p-6 h-[90vh]">
+                Tab content 3
+            </div>
+
+            <input type="radio" name="my_tabs_2" role="tab" class="tab bg-pink [--tab-bg:#F1C0E8] [--tab-border-color:transparent]" aria-label="" />
+            <div role="tabpanel" class="tab-content bg-pink p-6 h-[90vh]">
+                Tab content 4
+            </div>
+
+            <input type="radio" name="my_tabs_2" role="tab" class="tab bg-blue [--tab-bg:#A4C4F3] [--tab-border-color:transparent]" aria-label="" />
+            <div role="tabpanel" class="tab-content bg-blue p-6 h-[90vh]">
+                Tab content 5
+            </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+
+</style>
