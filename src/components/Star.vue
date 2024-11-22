2ba16881fd067c0066e4c9c1ce47bf66764354b2
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-11-21 19:07:31 -0600

    size

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,5 +1,5 @@
 <template>
-<svg class="w-[40px] h-[40px] md:w-[20px] md:h-[20px]" width="20" height="20" viewBox="0 0 48 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg class="w-[35px] h-[35px] md:w-[20px] md:h-[20px]" width="20" height="20" viewBox="0 0 48 46" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M24 3.23607L28.6618 17.5836L28.8863 18.2746H29.6129H44.6987L32.494 27.1418L31.9062 27.5689L32.1307 28.2599L36.7925 42.6074L24.5878 33.7401L24 33.3131L23.4122 33.7401L11.2075 42.6074L15.8693 28.2599L16.0938 27.5689L15.506 27.1418L3.30127 18.2746H18.3871H19.1137L19.3382 17.5836L24 3.23607Z" fill="url(#paint0_linear_78_1186)" stroke="black" stroke-width="2"/>
 <defs>
 <linearGradient id="paint0_linear_78_1186" x1="-1" y1="25.4771" x2="37.1579" y2="25.3928" gradientUnits="userSpaceOnUse">
