d8c38259833ab92d431c8749f61066b5a3f1efa0
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-11-21 19:55:29 -0600

    changed text

--- src/Sections/Maps.vue
+++ src/Sections/Maps.vue
@@ -75,8 +75,8 @@ const changeCountry = (string) => {
                 <div class="pt-2">
                     <h3 class="font-bold text-xs">From the southernmost part of Spain.</h3>
                     <p class="text-xs">
-                    Almeria is the humble crossroads between Africa and Europe. I miss the narrow streets, 
-                    the <span class="italic">jaleo</span> in the bars, its moorish charm, and the warm evenings in the mediterranean coast.</p>
+                    Almeria is the humble crossroads between Africa and Europe, Mediterranean at its core. I miss the narrow streets, 
+                    the <span class="italic">jaleo</span> in the bars, the warm evenings...</p>
                 </div>
             </swiper-slide>
             <swiper-slide class="flex p-5 items-center backdrop-blur-sm bg-white/50 w-[90%] flex-col h-full rounded-xl border-2 border-black">
@@ -85,7 +85,7 @@ const changeCountry = (string) => {
                     <h3 class="font-bold text-xs">London changed <span class="italic">everything</span>.</h3>
                     <p class="text-xs">
                     I didn’t speak English when I arrived, but I got a job cleaning tables and built a career in retail.
-                    Here, I made friends from all over the world, met my husband, and completely changed my life.
+                    Here, I made friends from all over the world, met my husband, and changed my life for ever.
                     </p>
                 </div>
             </swiper-slide>
@@ -107,14 +107,14 @@ const changeCountry = (string) => {
              <div v-if="country === 'spain'" class="pt-2 z-[99] col-start-1 col-span-3 row-start-1 row-span-3">
                 <h3 class="font-bold">From the southernmost part of Spain.</h3>
                 <p class="">
-                Almeria is the humble crossroads between Africa and Europe. I miss the narrow streets, 
-                the <span class="italic">jaleo</span> in the bars, its moorish charm, and the warm evenings in the mediterranean coast.</p>
+                    Almeria is the humble crossroads between Africa and Europe, Mediterranean at its core. I miss the narrow streets, 
+                    the <span class="italic">jaleo</span> in the bars, the warm evenings...</p>
             </div>
             <div v-if="country === 'uk'" class="pt-2 z-[99] col-start-1 col-span-3 row-start-1 row-span-3">
                 <h3 class="font-bold ">London changed <span class="italic">everything</span>.</h3>
                 <p class="">
                     I didn’t speak English when I arrived, but I got a job cleaning tables and built a career in retail.
-                    Here, I made friends from all over the world, met my husband, and completely changed my life.
+                    Here, I made friends from all over the world, met my husband, and changed my life for ever.
                 </p>
             </div>
             <div v-if="country === 'us'" class="pt-2 z-[99] col-start-1 col-span-3 row-start-1 row-span-3">
