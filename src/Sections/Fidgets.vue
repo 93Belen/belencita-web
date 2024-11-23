c97c1328452fc89d6684871a86b2e4fd76f417ab
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-11-23 15:42:39 -0600

    fixed something

--- src/Sections/Fidgets.vue
+++ src/Sections/Fidgets.vue
@@ -24,7 +24,7 @@ window.addEventListener('resize', () => {
        <div :role="mobile? 'tablist' : ''" :class="mobile ? 'tabs tabs-lifted grid-cols-5' : 'h-full md:grid grid-cols-3 grid-rows-4'">
            <input v-if="mobile" type="radio" name="my_tabs_2" role="tab" class="tab bg-purple tab-active [--tab-bg:#CEBAF0] [--tab-border-color:transparent]" aria-label="" />
             <div :role="mobile? 'tabpanel' : ''" :class="mobile? 'tab-content bg-purple p-6 h-[70vh]':'mockup-window bg-purple border'">
-                <div class="bg-[#FFFFF7] flex justify-center h-full px-4 py-16">
+                <div class="md:bg-[#FFFFF7] flex justify-center h-full px-4 py-16">
 
                 </div>
             </div>
@@ -36,19 +36,19 @@ window.addEventListener('resize', () => {
             </div>
             <input v-if="mobile" type="radio" name="my_tabs_2" role="tab" class="tab bg-orange [--tab-bg:#FDE3CE] [--tab-border-color:transparent]" aria-label="" />
             <div :role="mobile? 'tabpanel' : ''" :class="mobile? 'tab-content bg-orange p-6 h-[70vh]':'mockup-window bg-orange border row-span-4'">
-                <div class="bg-[#F4E5F1] flex justify-center h-full px-4 py-16">
+                <div class="md:bg-[#F4E5F1] flex justify-center h-full px-4 py-16">
 
                 </div>
             </div>
             <input v-if="mobile" type="radio" name="my_tabs_2" role="tab" class="tab bg-pink [--tab-bg:#F1C0E8] [--tab-border-color:transparent]" aria-label="" />
             <div :role="mobile? 'tabpanel' : ''" :class="mobile? 'tab-content bg-pink p-6 h-[70vh]':'mockup-window bg-pink border row-span-3'">
-                <div class="bg-[#ECE6F6] flex justify-center h-full px-4 py-16">
+                <div class="md:bg-[#ECE6F6] flex justify-center h-full px-4 py-16">
 
                 </div>
             </div>
             <input v-if="mobile" type="radio" name="my_tabs_2" role="tab" class="tab bg-blue [--tab-bg:#A4C4F3] [--tab-border-color:transparent]" aria-label="" />
             <div :role="mobile? 'tabpanel' : ''" :class="mobile? 'tab-content bg-blue p-6 h-[70vh]':'mockup-window bg-blue border row-span-2'">
-                <div class="bg-[#EAF0F9] flex justify-center h-full px-4 py-16"></div>
+                <div class="md:bg-[#EAF0F9] flex justify-center h-full px-4 py-16"></div>
             </div>
        </div>
     </div>
