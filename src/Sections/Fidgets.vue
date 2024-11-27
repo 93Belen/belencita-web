758df43e77e8cdbe5d74e972839699f9edbabc32
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-11-27 09:41:59 -0600

    one monster in mobile

--- src/Sections/Fidgets.vue
+++ src/Sections/Fidgets.vue
@@ -78,7 +78,7 @@ window.addEventListener('resize', () => {
        </div>
     </div>
     <!-- Monster -->
-    <div id="monster2" class="relative bottom-[220px] z-[9999] left-[100vw] w-[200px] h-[200px] hidden md:block opacity-0"><Monster2/></div>
+    <div id="monster2" class="relative md:bottom-[220px] z-[9999] left-[100vw] w-[50px] h-[50px] mt-[20px] md:w-[200px] md:h-[200px] opacity-0"><Monster2/></div>
 </div>
 
 </template>
