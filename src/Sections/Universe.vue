f7412b4fd62e9f9ba49387278b074034506eeeee
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-12-01 12:34:04 -0600

    added troy

--- src/Sections/Universe.vue
+++ src/Sections/Universe.vue
@@ -16,6 +16,7 @@ const names = [
     'frida',
     'blackwomen',
     'socks',
+    'troy',
     'chapstick',
     'hoodfeminism',
     'morente',
@@ -42,7 +43,8 @@ onMounted(() => {
         <div 
         v-for="(image, index) in names" :key='index' 
         :style="{transform: `rotate(${Math.random() * 30 - 15}deg)`, left: `${Math.random() * 10 - 5}wv`, top: `${Math.random() * 10 - 5}vh`}"
-        class="sticker w-[70px] h-[70px] md:w-[150px] md:h-[150px] relative">
+        :class="image === 'troy' ? 'w-[120px] h-[120px] md:w-[250px] md:h-[250px] col-start-1 md:col-start-3 md:row-start-2 z-[89]' : 'w-[70px] h-[70px] md:w-[150px] md:h-[150px]'"
+        class="sticker relative">
             <img width="auto" height="100%" :src="'/stickers/'+image+'.png'" alt="">
         </div>
         <div class="col-start-1 relative left-[-10vw] md:left-0 md:top-[-10vh] z-[0] col-span-3 row-start-2 z-[0] md:block md:col-start-4 md:row-start-1 md:col-span-3 md:row-span-2">
